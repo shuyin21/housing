0655e8192a96bc5f0f1238c357a0bb0d64d56a6a
shuyin21 <shuyindev@gmail.com>
2022-04-01 08:24:34 +0100

    appointment started

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FcOvertime } from 'react-icons/fc';
 
 const Appointment = () => {
     return (
         <AppointmentWrapper id='appointment'>
-            <h2>Appointment Component</h2>
+            <Container>
+                <Header>
+
+
+                    <AppointmentIcon />
+                    <h2>Book Appointment</h2>
+                </Header>
+            </Container>
         </AppointmentWrapper>
     )
 }
@@ -19,4 +27,33 @@ display:flex;
 flex-direction: column ;
 align-items:center;
 justify-content:center;
+`;
+
+
+const Container = styled.div`
+width:100%;
+height:100%;
+display:flex;
+flex-direction:column;
+
+`;
+const Header = styled.div`
+width:100%;
+height:150px;
+display:flex;
+flex-direction:column;
+align-items:center;
+justify-content:center;
+
+
+h2{
+    font-weight:100 ;
+}
+
+`;
+
+const AppointmentIcon = styled(FcOvertime)`
+width:40px;
+height:40px;
+
 `;
